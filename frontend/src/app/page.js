42293afe5cafa73bed06e9cd7ae3e34b9691cae3
Adrina Colaco <adcola13@gmail.com>
2025-12-16 16:49:20 +0530

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("/api/books") // Ensure correct API call
-    //axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/books`) // Ensure correct API call
+    //axios.get("/api/books") // Ensure correct API call
+    axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/books`) // Ensure correct API call
       .then(response => {
         setBooks(response.data);
         setLoading(false);
